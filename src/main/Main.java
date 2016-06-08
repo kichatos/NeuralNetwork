63ff3ba699f701a9b9961139d6f686355773bd74
kichatos <kichatos@gmail.com>
2016-06-08 16:44:38 +0300

    Added final training.

--- src/main/Main.java
+++ src/main/Main.java
@@ -23,46 +23,63 @@ public class Main {
      *             args[4] - line number //if only args[1] == "continue"
      */
     public static void main(String[] args) {
-        int fileNumber = 1;
-        int lineNumber = 0;
-        String unlabelledPath;
-        if (args.length > 0) {
-            unlabelledPath = args[0];
-            if (args.length == 5 && args[1].equals("continue")) {
-                fileNumber = Integer.valueOf(args[3]);
-                lineNumber = Integer.valueOf(args[4]);
-            }
-        } else {
-            unlabelledPath = "E:\\Wsl_F\\new_unlabelled";
+        List<Integer> neurons = Arrays.asList(9, 81, 53, 26);
+        DBN dbn = new DBN(neurons);
+//        DBN dbn = DBN.load("<path>.txt");
+
+        List<LabelledData> labelledDataList = new LabelledReader().next();
+        List<List<Double>> inputs = new ArrayList<>();
+        List<List<Double>> answers = new ArrayList<>();
+        for (LabelledData labelledData : labelledDataList) {
+            inputs.add(labelledData.getData());
+            answers.add(labelledData.getLabel());
         }
 
-        UnlabelledReader.setPath(unlabelledPath);
-        UnlabelledReader reader = new UnlabelledReader(fileNumber, lineNumber);
+        dbn.trainNetwork(inputs, answers);
 
-        DBN dbn;
-        if (args.length == 5 && args[1].equals("continue")) {
-            dbn = DBN.load(args[2]);
-        } else {
-            List<Integer> neurons = Arrays.asList(9, 81, 53, 26);
-            dbn = new DBN(neurons);
-        }
+        dbn.save("result.txt");
 
-        String lastPath = null;
-        while (reader.hasNext()) {
-            int currentFile = reader.getFileNumber();
-            int currentLine = reader.getLineNumber();
-
-            dbn.preTrainNetwork(reader.next());
-            String path = "" + currentFile + "-" + currentLine + ".txt";
-            dbn.save(path);
-            if (lastPath != null) {
-                boolean deleted = new File(lastPath).delete() && new File(lastPath + "net.txt").delete();
-                if (!deleted) {
-                    System.out.println("Failed to remove previous network.");
-                }
-            }
-
-            lastPath = path;
-        }
+        dbn.testClassifier(inputs, answers);
+//        int fileNumber = 1;
+//        int lineNumber = 0;
+//        String unlabelledPath;
+//        if (args.length > 0) {
+//            unlabelledPath = args[0];
+//            if (args.length == 5 && args[1].equals("continue")) {
+//                fileNumber = Integer.valueOf(args[3]);
+//                lineNumber = Integer.valueOf(args[4]);
+//            }
+//        } else {
+//            unlabelledPath = "E:\\Wsl_F\\new_unlabelled";
+//        }
+//
+//        UnlabelledReader.setPath(unlabelledPath);
+//        UnlabelledReader reader = new UnlabelledReader(fileNumber, lineNumber);
+//
+//        DBN dbn;
+//        if (args.length == 5 && args[1].equals("continue")) {
+//            dbn = DBN.load(args[2]);
+//        } else {
+//            List<Integer> neurons = Arrays.asList(9, 81, 53, 26);
+//            dbn = new DBN(neurons);
+//        }
+//
+//        String lastPath = null;
+//        while (reader.hasNext()) {
+//            int currentFile = reader.getFileNumber();
+//            int currentLine = reader.getLineNumber();
+//
+//            dbn.preTrainNetwork(reader.next());
+//            String path = "" + currentFile + "-" + currentLine + ".txt";
+//            dbn.save(path);
+//            if (lastPath != null) {
+//                boolean deleted = new File(lastPath).delete() && new File(lastPath + "net.txt").delete();
+//                if (!deleted) {
+//                    System.out.println("Failed to remove previous network.");
+//                }
+//            }
+//
+//            lastPath = path;
+//        }
     }
 }
